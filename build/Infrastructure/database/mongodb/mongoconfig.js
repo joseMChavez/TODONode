6985110d4cfeda0688f02802c20d98c726e8d732
JOSE <josemchavez0123@hotmail.com>
2022-02-24 15:20:00 -0400

    yes

--- build/Infrastructure/database/mongodb/mongoconfig.js
+++ build/Infrastructure/database/mongodb/mongoconfig.js
@@ -6,11 +6,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose_1 = __importDefault(require("mongoose"));
 class MongoDBConect {
     Access() {
-        const uri = "mongodb://localhost:27017/TodoDb"; ///= `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@clustertodo.raz9g.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
+        const uri = process.env.MONGO_CON_STR || ""; ///= `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@clustertodo.raz9g.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
         //const options = { useNewUrlParser: true, useUnifiedTopology: true }
         //mongoose.set("useFindAndModify", false)
         mongoose_1.default.connect(uri)
-            .then(() => console.log("Estamos conectados..."))
+            .then(() => console.log("We are connected..."))
             .catch(error => {
             throw error;
         });
--- src/Infrastructure/database/mongodb/mongoconfig.ts
+++ src/Infrastructure/database/mongodb/mongoconfig.ts
@@ -1,13 +1,15 @@
 import mongoose from "mongoose"
+
 export default class MongoDBConect{
   public Access (){
-    const uri: string ="mongodb://localhost:27017/TodoDb";///= `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@clustertodo.raz9g.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
+
+    const uri: string =process.env.MONGO_CON_STR || "";///= `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@clustertodo.raz9g.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
     //const options = { useNewUrlParser: true, useUnifiedTopology: true }
     //mongoose.set("useFindAndModify", false)
     
      mongoose.connect(uri)
       .then(() =>
-        console.log("Estamos conectados...")
+        console.log("We are connected...")
       )
       .catch(error => {
         throw error
